cb76d0c254f3f98518ac5426571c9b870ddd4dab
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-06-18 20:27:16 +0530

    style: adjust indentation in main.tsx for consistency

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import { Providers } from "./context/Providers.tsx";
 
 createRoot(document.getElementById("root")!).render(
-   <StrictMode>
+  <StrictMode>
     <Providers>
       <App />
     </Providers>
